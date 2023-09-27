523fe59768346b59741789cc4bad76f0f1e143e8
NPworld <pnaveen0698@gmail.com>
2023-09-27 20:10:17 +0530

    Add a countdown

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
+const launchTime = new Date('09/26/2023 02:25').getTime()
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  const currentTime = new Date().getTime()
+  const numberOfMilliseconds = parseInt(launchTime - currentTime)
+  res.render(
+    'index',
+  { title: 'My Awesome Game go Better', countDown: numberOfMilliseconds  });
 });
 
 module.exports = router;
